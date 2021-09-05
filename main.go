e69bdd77c489376ab13e1293b4897e259f45a8c7
linzihong <linzihong@coding.net>
2021-09-05 20:43:59 +0800

    day 1 over

--- main.go
+++ main.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	"gee"
 )
 
-func main(){
-	http.HandleFunc("/", indexHandle)
-	http.HandleFunc("/header", helloHandle)
-	log.Fatal(http.ListenAndServe(":9999", nil))
-}
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
 
-func indexHandle(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "this is index")
-}
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
 
-func helloHandle(w http.ResponseWriter, req *http.Request) {
-	for k,v := range req.Header {
-		fmt.Fprintf(w, "Header[%q]=%q\n", k, v)
-	}
+	r.Run(":9999")
 }
-
